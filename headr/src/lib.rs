8e7b4ffc5526a474ab88c4c88e591da434c27c3d
mahiro-1 <riceomeletomuraisu@gmail.com>
2025-10-27 14:23:08 +0900

    removing tests directry

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -19,19 +21,70 @@ pub fn get_args() -> MyResult<Config> {
         .about("Rust head")
         .arg(
             Arg::with_name("files")
-                
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines [default: 10]")
+                .default_value("10")
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Number of lines")
+                .takes_value(true)
+                .value_name("BYTES")
+                .conflicts_with("lines")
         )
         .get_matches();
+    /* 失敗作
+    let lines_val = parse_positive_int(matches.value_of("lines").unwrap());
+    let bytes_val = parse_positive_int(matches.value_of("bytes").unwrap());
+    if(lines_val.is_err()){
+        return Err(lines_val);
+    }
+    if(bytes_val.is_err()){
+        return Err(bytes_val);
+    }
+    Ok(Config {
+        files: ,
+        lines: lines_val.unwrap(),
+        bytes: bytes_val.unwrap(),
+    })
+    */
+    let lines = matches
+        .value_of("lines") //value_ofでOption<&str>を取得
+        .map(parse_positive_int) //SomeだったものをOptionから取り出し、関数に流す
+        .transpose() //Option<Result>をResult<Option>に変換する
+        .map_err(|e| format!("illegal line count -- {}", e))?; //エラーなら中身のエラーメッセージを返し、OKならそのまま、?を使ってエラー伝播かOkをアンパックする
 
-    Ok(
-        files: aaa,
-        lines: aaa,
-        bytes: aaa,
-    )
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes //もともとOption型だからそのまま書いてキーを省略する
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
 
@@ -69,4 +122,11 @@ fn test_parse_povitive_int() {
     let res = parse_positive_int("0");
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
