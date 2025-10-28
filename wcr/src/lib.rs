adaa4b8f8b4dd8d55368d50b18a9625d390359d7
mahiro-1 <riceomeletomuraisu@gmail.com>
2025-10-28 10:32:16 +0900

    add about wc

---
+++ wcr/src/lib.rs
@@ -0,0 +1,55 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("mahiro-1")
+        .about("Rust wc")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("Show line count")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("Show word count")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Show byte count")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("Show character count")
+                .takes_value(false)
+        )
+}
